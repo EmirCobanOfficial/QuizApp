2d7a9e39c7eb9223411a0576ac0555ea57b95826
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-05-22 16:06:50 +0300

    Update app.js

--- QuizApp/js/app.js
+++ QuizApp/js/app.js
@@ -2,13 +2,13 @@ const quiz = new Quiz(soruListesi);
 const ui = new UI();
 
 ui.btnStart.addEventListener("click", function () {
+    startTimer(10);
     ui.quizBox.classList.add("active");
     ui.buttonBox.classList.remove("active");
     ui.soruGoster(quiz.soruGetir());
     ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
-
+    ui.btnNext.classList.remove("show");
 });
-
 
 ui.soruGoster(quiz.soruGetir());
 ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
@@ -16,8 +16,10 @@ ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
 ui.btnNext.addEventListener("click", function () {
     quiz.soruIndex++;
     if (quiz.soruIndex < quiz.sorular.length) {
+        startTimer(10);
         ui.soruGoster(quiz.soruGetir());
         ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+        ui.btnNext.classList.remove("show");
     } else {
         ui.scoreBox.classList.add("active");
         ui.quizBox.classList.remove("active");
@@ -26,6 +28,7 @@ ui.btnNext.addEventListener("click", function () {
 });
 
 function optionSelected(e) {
+    clearInterval(counter);
     let selectedElement = e.target;
 
     if (selectedElement.nodeName === "SPAN") {
@@ -45,7 +48,14 @@ function optionSelected(e) {
     }
 
     ui.disableAllOption();
+    ui.btnNext.classList.add("show");
 }
+
+quiz.soruIndex += 1;
+ui.disableAllOption();
+ui.btnNext.classList.add("show");
+
+ui.disableAllOption();
 
 ui.btnQuit.addEventListener("click", function () {
     window.location.reload();
@@ -59,3 +69,22 @@ ui.btnReplay.addEventListener("click", function () {
     ui.btnStart.click();
     ui.scoreBox.classList.remove("active");
 });
+
+let counter;
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+
+    function timer() {
+        ui.timeSecond.textContent = time;
+        time--;
+
+        if (time < 0) {
+            clearInterval(counter);
+            ui.timeText.textContent = "Süre Bitti";
+
+            ui.disableAllOption();
+
+            ui.btnNext.classList.add("show");
+        }
+    }
+}
