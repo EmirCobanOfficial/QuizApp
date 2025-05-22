74f10469a3d34ad8acbc719cb2a25f38c9dfddc2
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-05-22 14:42:24 +0300

    Update app.js

--- QuizApp/js/app.js
+++ QuizApp/js/app.js
@@ -2,27 +2,36 @@ const quiz = new Quiz(soruListesi);
 const ui = new UI();
 
 
-document.getElementById("btnSoruGetir").addEventListener("click", function () {
-    if (quiz.sorular.length != quiz.soruIndex) {
+ui.soruGoster(quiz.soruGetir());
+ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+
+ui.btnNext.addEventListener("click", function () {
+    quiz.soruIndex++;
+    if (quiz.soruIndex < quiz.sorular.length) {
         ui.soruGoster(quiz.soruGetir());
-        console.log(quiz);
-    }
-    else {
+        ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+    } else {
         console.log("quiz bitti");
     }
 });
 
 function optionSelected(e) {
     const cevap = e.target.textContent[0];
+
+    let selectedElement = e.target;
+    if (selectedElement.nodeName === "SPAN") {
+        selectedElement = selectedElement.parentElement;
+    }
+
     const soru = quiz.soruGetir();
 
     if (soru.cevabiKontrolEt(cevap)) {
-        e.target.classList.add("correct");
-        e.target.insertAdjacentHTML("beforeend", ui.correctIcon);
+        selectedElement.classList.add("correct");
+        selectedElement.insertAdjacentHTML("beforeend", ui.correctIcon);
     } else {
-        e.target.classList.add("incorrect");
-        e.target.insertAdjacentHTML("beforeend", ui.inCorrectIcon);
+        selectedElement.classList.add("incorrect");
+        selectedElement.insertAdjacentHTML("beforeend", ui.inCorrectIcon);
     }
-    quiz.soruIndex += 1;
+
     ui.disableAllOption();
 }
